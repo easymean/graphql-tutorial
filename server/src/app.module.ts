634b379a3ea663367be65ac6bbe72128e3a28519
easymean <easymean@ncsoft.com>
2025-01-15 18:17:15 +0900

    nest config 추가

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -3,9 +3,14 @@ import { GraphQLModule } from '@nestjs/graphql';
 import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { DirectiveLocation, GraphQLDirective } from 'graphql';
 import { PostModule } from './post/post.module';
+import { ConfigModule } from '@nestjs/config';
+import configuration from 'src/config/configuration';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      load: [configuration],
+    }),
     GraphQLModule.forRoot<ApolloDriverConfig>({
       driver: ApolloDriver,
       autoSchemaFile: 'schema.gql',
@@ -18,6 +23,7 @@ import { PostModule } from './post/post.module';
           }),
         ],
       },
+      path: '/graphql',
     }),
     PostModule,
   ],
