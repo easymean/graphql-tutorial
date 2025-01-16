2add6e6e669388e63fb1d058ba50585353f53a1d
easymean <easymean@ncsoft.com>
2025-01-16 13:10:10 +0900

    config 삭제하고 main에서 port, cors 관리

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -3,14 +3,9 @@ import { GraphQLModule } from '@nestjs/graphql';
 import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { DirectiveLocation, GraphQLDirective } from 'graphql';
 import { PostModule } from './post/post.module';
-import { ConfigModule } from '@nestjs/config';
-import configuration from 'src/config/configuration';
 
 @Module({
   imports: [
-    ConfigModule.forRoot({
-      load: [configuration],
-    }),
     GraphQLModule.forRoot<ApolloDriverConfig>({
       driver: ApolloDriver,
       autoSchemaFile: 'schema.gql',
