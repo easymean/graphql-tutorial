b1d9242c8a73fbd6e2e69c05ab6bb5223e474bc6
easymean <easymean@ncsoft.com>
2025-01-16 13:50:13 +0900

    id에 해당하는 blog 조회: sc, cc 버전

--- client/app/blog/[id]/page.tsx
+++ client/app/blog/[id]/page.tsx
@@ -1,4 +1,6 @@
-import { getPostQuery } from '@/app/lib/actions';
+import { query } from '@/lib/apllo/ApolloClient';
+import { GET_POST } from '@/lib/queries';
+import { Post } from '@/types/post';
 
 type Props = {
   id: string;
@@ -6,10 +8,23 @@ type Props = {
 
 export default async function Page({ params }: { params: Promise<Props> }) {
   const { id } = await params;
-  const { data } = await getPostQuery(id);
+  const { data, loading, error } = await query<{ post: Post }, { id: string }>({
+    query: GET_POST,
+    variables: { id },
+  });
+
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p> Error: {error.message}</p>;
+
+  const { post } = data!;
+
   return (
     <div>
-      <p>{data.post.title}</p>
+      <h1>Post</h1>
+      <div>
+        <h2>{post.title}</h2>
+        <p>{post.content}</p>
+      </div>
     </div>
   );
 }
---
+++ client/app/blog/[id]/post.tsx
@@ -0,0 +1,30 @@
+// client component에서 graphql 사용 예시입니다.
+
+'use client';
+
+import { GET_POST } from '@/lib/queries';
+import { Post } from '@/types/post';
+import { useQuery } from '@apollo/client';
+import { useParams } from 'next/navigation';
+
+export default function PostDetail() {
+  const { id } = useParams() as { id: string };
+  const { data, loading, error } = useQuery<
+    {
+      post: Post;
+    },
+    { id: string }
+  >(GET_POST, { variables: { id } });
+
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p> Error: {error.message}</p>;
+
+  const { post } = data!;
+
+  return (
+    <div>
+      <h2>{post.title}</h2>
+      <p>{post.content}</p>
+    </div>
+  );
+}
